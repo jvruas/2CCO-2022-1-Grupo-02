11aee0e0956356a784fb2bc3aecbe4c089c40d6d
Matheus Franciscone <matheus.cunha@bandtec.com.br>
2022-03-12 14:43:55 -0300

    Add spring API login/logoff

---
+++ api-spring/conture/src/test/java/com/example/conture/ContureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
