fd8b0a831d25d4afb64378753e76788f7103d6ed
Medeiros <gabriel.r.medeiros@accenture.com>
2022-03-15 21:46:03 -0300

    atualizações site

--- Site/script.js
+++ Site/script.js
@@ -1,13 +1,64 @@
-window.onscroll = function(){
-    scroll();
-}
-const $html = document.querySelector('html')
+var header = $('#header');
+var logoheader = $('#header img');
+$(document).scroll(function() {
+  var scroll = $(this).scrollTop();
 
-function scroll (){    
-    if(document.documentElement.scrollTop>90){
-        a_titulo.style.color = "black";      
-    }   else {
-        a_titulo.style.color = "white";
+if (scroll > 400) {
+    header.removeClass('home');
+    logoheader.attr('src', './img/logo black.png');
+} else {
+    header.addClass('home');
+    logoheader.attr('src', './img/Full white logo.png');
+}
+
+if(scroll<400){
+    bold(1)
+}
+else if(scroll > 400 && scroll < 800){
+    bold(2);
+}
+else if(scroll > 800 && scroll < 1400){
+    bold(3);
+}
+else if(scroll>1500){
+    bold(4)
+}
+
+});
+
+function bold(number){
+    var home = $('#a_home');
+    var sobre = $('#a_sobre');
+    var historia = $('#a_historia');
+    var contato = $('#a_contato');
+
+    switch(number){
+        case 1:
+            home.addClass('nav-link-selected');
+            sobre.removeClass('nav-link-selected');
+            historia.removeClass('nav-link-selected');
+            contato.removeClass('nav-link-selected');
+            break;
+
+        case 2:
+            home.removeClass('nav-link-selected');
+            sobre.addClass('nav-link-selected');
+            historia.removeClass('nav-link-selected');
+            contato.removeClass('nav-link-selected');
+            break;
+
+        case 3:
+            home.removeClass('nav-link-selected');
+            sobre.removeClass('nav-link-selected');
+            historia.addClass('nav-link-selected');
+            contato.removeClass('nav-link-selected');
+            break;
+
+        case 4:
+            home.removeClass('nav-link-selected');
+            sobre.removeClass('nav-link-selected');
+            historia.removeClass('nav-link-selected');
+            contato.addClass('nav-link-selected');
+            break;
     }
 }
-
