e70dfb3d09f827f2757c55075f76daaf2316213b
IgorSergio2003 <igor.souza@bandtec.com.br>
2022-04-24 22:57:44 -0300

    arrumando

---
+++ app/backend/api-produto/src/main/java/com/conture/apiproduto/SearchProdutoCategoriaIterator.java
@@ -0,0 +1,9 @@
+package com.conture.apiproduto;
+
+import com.conture.apiproduto.entity.ProdutoDoacao;
+
+public class SearchProdutoCategoriaIterator {
+	private ListaObj<ProdutoDoacao> collection;
+
+	private int iterationState;
+}
