888bcaf420266f9f0f4c6241c10a3f5e2dcf4cec
IgorSergio2003 <igor.souza@bandtec.com.br>
2022-04-24 21:55:58 -0300

    Implementando na api produto

--- app/backend/api-produto/src/main/java/com/conture/apiproduto/SearchProdutoCategoriaIterator.java
+++ app/backend/api-produto/src/main/java/com/conture/apiproduto/SearchProdutoCategoriaIterator.java
@@ -1,4 +1,11 @@
 package com.conture.apiproduto;
 
+import com.conture.apiproduto.entity.ProdutoDoacao;
+
+import java.util.List;
+
 public class SearchProdutoCategoriaIterator {
+	private ListaObj<ProdutoDoacao> collection;
+
+	private int iterationState;
 }
--- app/backend/api-produto/src/main/java/com/conture/apiproduto/repository/MatchRepository.java
+++ app/backend/api-produto/src/main/java/com/conture/apiproduto/repository/MatchRepository.java
@@ -1,8 +1,10 @@
 package com.conture.apiproduto.repository;
 
 import com.conture.apiproduto.entity.Match;
+import com.conture.apiproduto.entity.ProdutoDoacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,8 +20,10 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
 
 	Match findByFkDoadorAndFkProdutoDoacaoAndFkDonatario(Long fkDoador, Long fkProdutoDoacao, Long fkDonatario);
 
-//	@Transactional
-//	@Modifying
-//	@Query("update Match m set m.status = true where m.fkDoador = ?1 and m.fkProdutoDoacao = ?2 and m.fkDonatario = ?3")
-//	void updateMatchSetStatus(Long fkDoador, Long fkProdutoDoacao, Long fkDonatario );
+	@Transactional
+	@Modifying
+	@Query("update Match m set m.status = true where m.fkDoador = ?1 and m.fkProdutoDoacao = ?2 and m.fkDonatario = ?3")
+	void updateMatchSetStatus(Long fkDoador, Long fkProdutoDoacao, Long fkDonatario );
+
+	List<Match> findByFkDoadorAndStatus(Long fkDoador, String status);
 }
