418bc8fac589cadd45da38e9ff892fb55e564e60
João Ruas <jvruas>
2022-04-24 23:15:03 -0300

    Retirando teste

--- app/api-produto/src/main/java/com/conture/apiproduto/SearchProdutoCategoriaIterator.java
+++
@@ -1,9 +0,0 @@
-package com.conture.apiproduto;
-
-import com.conture.apiproduto.entity.ProdutoDoacao;
-
-public class SearchProdutoCategoriaIterator {
-	private ListaObj<ProdutoDoacao> collection;
-
-	private int iterationState;
-}
--- app/api-produto/src/main/java/com/conture/apiproduto/repository/PreferenciaDonatarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.conture.apiproduto.repository;
-
-import com.conture.apiproduto.entity.PreferenciaDonatario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreferenciaDonatarioRepository extends JpaRepository<PreferenciaDonatario, Long> {
-}
