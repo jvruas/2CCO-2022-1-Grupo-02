81e14696d789f39a77aa15a646ae9ac78fc9d486
Medeiros <gabriel.r.medeiros@accenture.com>
2022-05-28 01:23:28 -0300

    Atualização da importação

--- app/backend/api-produto/src/main/java/com/conture/apiproduto/utility/Txt.java
+++ app/backend/api-produto/src/main/java/com/conture/apiproduto/utility/Txt.java
@@ -6,9 +6,9 @@ import com.conture.apiproduto.repository.CategoriaProdutoRepository;
 import com.conture.apiproduto.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -21,7 +21,49 @@ public class Txt {
 	@Autowired
 	private ProdutoRepository repositoryProduto;
 
+	public void gravaRegistro(String registro, String nomeArq) {
+		BufferedWriter saida = null;
+
+		// try-catch para abrir o arquivo
+		try {
+			saida = new BufferedWriter(new FileWriter(nomeArq, true));
+		}
+		catch (IOException erro) {
+			System.out.println("Erro ao abrir o arquivo: " + erro);
+		}
+
+		// try-catch para gravar o registro e fechar o arquivo
+		try {
+			saida.append(registro + "\n");
+			saida.close();
+		}
+		catch (IOException erro) {
+			System.out.println("Erro ao gravar o arquivo: " + erro);
+		}
+	}
+
+	public void gravaArquivoTxt(String corpo, String nomeArq) {
+		int contaRegCorpo = 0;
+
+		// Monta o registro de header
+		String header = "HDPRODUTO";
+		header += LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+		header += "01";
+		// Grava o registro de header
+		gravaRegistro(header, nomeArq);
+
+		// Monta e grava os registros de corpo
+		gravaRegistro(corpo, nomeArq);
+
+
+		// Monta e grava o registro de trailer
+		String trailer = "TR";
+		trailer += String.format("%010d", contaRegCorpo);
+		gravaRegistro(trailer, nomeArq);
+	}
+
 	public Boolean leArquivoTxt(String nomeArq, String registro) {
+		gravaArquivoTxt(registro, nomeArq);
 		BufferedReader entrada = null;
 		Long fkDoador;
 		String tipoRegistro;
@@ -29,11 +71,10 @@ public class Txt {
 		Boolean defeito, entrega;
 		Long fkCategoria;
 		int contaRegCorpoLido = 0;
-		int qtdRegCorpoGravado;
+		int qtdRegCorpoGravado = 10;
 
 		List<ProdutoDoacao> listaLida = new ArrayList<>();
 
-		// try-catch para abrir o arquivo
 		try {
 			entrada = new BufferedReader(new FileReader(nomeArq));
 		}
@@ -41,28 +82,32 @@ public class Txt {
 			System.out.println("Erro ao abrir o arquivo: " + erro);
 		}
 
+		// try-catch para abrir o arquivo
 		// try-catch para ler e fechar o arquivo
-		try {
 			// Leitura do primeiro registro do arquivo
+		try {
+
+			registro = entrada.readLine();
 
 			while (registro != null) { // enquanto não chegou ao final do arquivo
 				// obtém os 2 primeiros caracteres do registro
 				// 01234567
 				// 00NOTA20221
-				tipoRegistro = registro.substring(0,2);
+				if (contaRegCorpoLido == 0) {
+					tipoRegistro = registro.substring(0, 2);
+				} else {
+					tipoRegistro = registro.substring(0, 4);
+				}
 				if (tipoRegistro.equals("HD")) {
 					System.out.println("É um registro de header");
-					System.out.println("Tipo de arquivo: " + registro.substring(2,9));
-					System.out.println("Data e hora da gravação: " + registro.substring(9,28));
-					System.out.println("Versão do documento: " + registro.substring(28,30));
+					System.out.println("Tipo de arquivo: " + registro.substring(2, 9));
+					System.out.println("Data e hora da gravação: " + registro.substring(9, 28));
+					System.out.println("Versão do documento: " + registro.substring(28, 30));
+					contaRegCorpoLido++;
+				} else if (tipoRegistro.equals("TR")) {
+					System.out.println("É um registro de trailer");
+					qtdRegCorpoGravado = Integer.parseInt(registro.substring(2, 12));
 				}
-				//else if (tipoRegistro.equals("TR")) {
-				//	System.out.println("É um registro de trailer");
-				//	qtdRegCorpoGravado = Integer.parseInt(registro.substring(2,12));
-				//	if (contaRegCorpoLido == qtdRegCorpoGravado) {
-				//		System.out.println("Quantidade de registros lidos é compatível " +
-				//				"com a quantidade de registros gravados");
-				//	}
 				//	else {
 				//		System.out.println("Quantidade de registros lidos não é compatível " +
 				//				"com a quantidade de registros gravados");
@@ -70,19 +115,23 @@ public class Txt {
 				//}
 				else if (tipoRegistro.equals("CP02")) {
 					System.out.println("É um registro de corpo");
-					fkDoador = Long.valueOf(registro.substring(4,8));
-					nome = registro.substring(8,68).trim();
-					marca = registro.substring(68,128).trim();
-					modelo = registro.substring(128,188).trim();
-					categoria = registro.substring(188,208).trim();
-					defeito = Boolean.valueOf(registro.substring(208,209));
-					entrega = Boolean.valueOf(registro.substring(210,211));
-					descricao = registro.substring(211,271).trim();
+					System.out.println(registro);
+					fkDoador = Long.valueOf(registro.substring(4, 8));
+					nome = registro.substring(8, 68).trim();
+					marca = registro.substring(68, 128).trim();
+					modelo = registro.substring(128, 188).trim();
+					categoria = registro.substring(188, 208).trim();
+					defeito = Boolean.valueOf(registro.substring(208, 209));
+					entrega = Boolean.valueOf(registro.substring(210, 211));
+					descricao = registro.substring(211, 271).trim();
+
 					contaRegCorpoLido++;
 
-					Optional<CategoriaProduto> categoriaEncontrada =
-							repositoryCategoria
-									.findByNomeIgnoreCase(categoria);
+					System.out.println(marca);
+					System.out.println(modelo);
+					System.out.println(categoria);
+
+					Optional<CategoriaProduto> categoriaEncontrada = repositoryCategoria.findByNomeIgnoreCase(categoria);
 
 					fkCategoria = categoriaEncontrada.get().getIdCategoriaProduto();
 
@@ -99,16 +148,14 @@ public class Txt {
 
 					// No nosso caso, vamos adicionar o objeto a na listaLida:
 					listaLida.add(produtoDoacao);
-				}
-				else {
+				} else {
+					System.out.println(registro.substring(0, 4));
 					System.out.println("Tipo de registro inválido!");
 				}
 
-				// Lê o próximo registro
 				registro = entrada.readLine();
+				qtdRegCorpoGravado--;
 			}
-
-			entrada.close();
 		}
 		catch (IOException erro) {
 			System.out.println("Erro ao ler o arquivo: " + erro);
@@ -124,6 +171,7 @@ public class Txt {
 				System.out.println(p);
 				repositoryProduto.save(p);
 			}
+			System.out.println(listaLida);
 			return true;
 		}
 		catch (IllegalStateException e){
