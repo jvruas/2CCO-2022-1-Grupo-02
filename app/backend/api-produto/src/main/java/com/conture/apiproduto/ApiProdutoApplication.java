4f6a75775890fbf40580209dcefc2e0f5fbc11c4
João Ruas <jvruas>
2022-06-03 21:01:17 -0300

    Resolvendo problema da API produto

--- app/backend/api-produto/src/main/java/com/conture/apiproduto/ApiProdutoApplication.java
+++ app/backend/api-produto/src/main/java/com/conture/apiproduto/ApiProdutoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class ApiProdutoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiProdutoApplication.class, args);
 	}
-
 }
