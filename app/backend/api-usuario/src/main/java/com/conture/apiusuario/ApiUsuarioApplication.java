5907510695284e769b747446a37da4cdb9778625
João Ruas <jvruas>
2022-06-05 22:35:45 -0300

    Removendo AvaliacaoRequest da API de usuario

--- app/backend/api-usuario/src/main/java/com/conture/apiusuario/ApiUsuarioApplication.java
+++ app/backend/api-usuario/src/main/java/com/conture/apiusuario/ApiUsuarioApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class
 
 ApiUsuarioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiUsuarioApplication.class, args);
 	}
