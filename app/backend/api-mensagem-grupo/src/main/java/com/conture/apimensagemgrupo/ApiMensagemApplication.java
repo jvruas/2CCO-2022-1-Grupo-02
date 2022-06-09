14d5ca615440070f3b9da0bf0e036b28cfe6dd15
João Ruas <jvruas>
2022-06-08 21:19:27 -0300

    Configurando Application Properties para API Mensagem Grupo

--- app/backend/api-mensagem-grupo/src/main/java/com/conture/apimensagemgrupo/ApiMensagemApplication.java
+++ app/backend/api-mensagem-grupo/src/main/java/com/conture/apimensagemgrupo/ApiMensagemApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class ApiMensagemApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiMensagemApplication.class, args);
 	}
-
 }
