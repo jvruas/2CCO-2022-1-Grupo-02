4e53c58fdb4690d452985107377378680a2916d1
Matheus Franciscone <matheus.cunha@bandtec.com.br>
2022-10-19 17:21:15 -0300

    BI quase finalizado

--- app/frontend/conture-site/src/pages/Dashboard.jsx
+++ app/frontend/conture-site/src/pages/Dashboard.jsx
@@ -1,5 +1,21 @@
 import SideBar from "../components/SideBar";
 import "../html-css-template/css/Dashboard.css";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Year", "Sales", "Expenses", "Profit"],
+  ["2014", 1000, 400, 200],
+  ["2015", 1170, 460, 250],
+  ["2016", 660, 1120, 300],
+  ["2017", 1030, 540, 350],
+];
+
+export const options = {
+  chart: {
+    title: "Company Performance",
+    subtitle: "Sales, Expenses, and Profit: 2014-2017",
+  },
+};
 
 function Dashboard() {
   return (
@@ -12,13 +28,40 @@ function Dashboard() {
           </div>
           <hr />
           <div className="container-superior">
-            <div className="card-view">C</div>
-            <div className="card-view">U</div>
+          <Chart
+              chartType="Bar"
+              width="70%"
+              height="400px"
+              data={data}
+              options={options}
+            />
+
+            <Chart
+              chartType="Bar"
+              width="10%"
+              height="1000px"
+              data={data}
+              options={options}
+            />
+
           </div>
 
           <div className="container-inferior">
-            <div className="card-view">PIRO</div>
-            <div className="card-view">CA</div>
+            <Chart
+              chartType="Bar"
+              width="70%"
+              height="400px"
+              data={data}
+              options={options}
+            />
+
+            <Chart
+              chartType="Bar"
+              width="70%"
+              height="400px"
+              data={data}
+              options={options}
+            />
           </div>
         </div>
       </section>
