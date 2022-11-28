e54ce557baa7f8c3ed81a0c0f1cc9311924cafb2
Flávia de Oliveira <flavia_gudy@hotmail.com>
2022-11-27 21:07:04 -0300

    Apagado o comentario do popUp report 2

--- app/frontend/conture-site/src/pages/pop-up/PopUpReporte2.jsx
+++ app/frontend/conture-site/src/pages/pop-up/PopUpReporte2.jsx
@@ -36,25 +36,6 @@ function PopUpFiltro() {
         <> <section id='filtro'>
             <div className='div_filtro'>
                 <div className='div_sup_filtro'><p>Confirmar reporte</p><Link to={`/disponivel `}><img src={fechar} alt="" /></Link></div>
-                {/* <div className='div_meio_filtro'>
-                    <div>
-                        <input type="checkbox" name="horns" id='0' onClick={() => setIdSelecionado(0)}/>
-                        <label for="horns">Todos</label></div>
-                    <div><input type="checkbox" name="horns" id='1' onClick={() => setIdSelecionado(1)}/>
-                        <label for="horns">Celular</label></div>
-                    <div><input type="checkbox" name="horns" id='2' onClick={() => setIdSelecionado(2)}/>
-                        <label for="horns">Computador</label></div>
-                    <div><input type="checkbox" name="horns" id='3' onClick={() => setIdSelecionado(3)}/>
-                        <label for="horns">Headset</label></div>
-                    <div><input type="checkbox" name="horns" id='4' onClick={() => setIdSelecionado(4)}/>
-                        <label for="horns">Notebook</label></div>
-                        <div><input type="checkbox" name="horns" id='5' onClick={() => setIdSelecionado(5)}/>
-                        <label for="horns">Tablet</label>
-
-<button className='btn_filtrar'>Confirmar</button>
-                        </div>
-                </div> */}
-
                 <div className='div_inf'><button className='btn_filtrar' onClick={(() => {handleSubmit()})}>Confirmar</button></div>
             </div>
             </section>
