65bc69aa5f28413cdf24a59585254948fff6038c
IgorSergio2003 <Igor.souza@bandtec.com.br>
2022-12-01 07:11:31 -0300

    Adicionando match

--- app/frontend/conture-site/src/components/CardMatch.jsx
+++ app/frontend/conture-site/src/components/CardMatch.jsx
@@ -4,54 +4,68 @@ import fotoDeslogado from '../html-css-template/imagens/imagem-deslogado.png';
 import aceitar from '../html-css-template/imagens/accept.svg';
 import rejeitar from '../html-css-template/imagens/reject.svg';
 import '../html-css-template/css/CardMatch.css'
+import apiProdutos from '../apiProduto';
+import { useNavigate } from 'react-router-dom';
 // import Header from './headerMatch';
 
-function CardMatch() {
+
+
+function CardMatch(props) {
+
+const navegar = useNavigate();  
+
+
+    function aceitarMatch(){
+        apiProdutos.patch(`26?idDoador=6&idDonatario=7`).then((resposta) => {
+            // sessionStorage.setItem("produtoAvaliado","23")
+            navegar("/popup-avaliacao")
+        }).catch((error) => { 
+                console.log(error)
+         })
+
+    }
+
+    function retirarMatch() {
+        apiProdutos.delete(`28/match?idDonatario=7`,
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }).then((resposta) => {
+            //console.log(resposta.status)
+            navegar("/")
+            // document.location.reload(true)
+          }).catch((error) => {
+            console.log(error)
+          })
+      }
     return (
         <>
             <div className='div_match'>
                 <div className='perfil_inf'>
                     <div className='div_imagem'>
-                        <img src={fotoDeslogado} alt="" />
+                        <img src={props.foto} alt="" />
                     </div>
                     <div className='div_informacao'>
                         <div className='inf_sup'><b>
-                            <p>Igor</p>
+                            <p>{props.nome}</p>
                         </b></div>
-                        <div className='inf_meio'><p>São Paulo - SP</p>
-                            <p>10/08/21</p></div>
+                        <div className='inf_meio'><p>{props.cidade} - {props.uf}</p>
+                            </div>
                         <div className='div_inf'><img src={estrela} alt="" />
-                            <p>3.0</p></div>
+                            <p>{props.nota}</p></div>
                     </div>
-
-                    {/* <div classname="foto_match">
-                                    <img src={fotoDeslogado} alt="" />
-                                </div>
-                                <div classname='perfil_texto_match'>
-                                <div classname="inf_match">
-                                        <b>
-                                            <p>Igor</p>
-                                        </b>
-                                    </div>
-                                    <div classname='inf'>
-                                        <p>São Paulo - SP</p>
-                                        <p>10/08/21</p>
-                                    </div>
-                                    <div className=''>
-                                        <img src={estrela} alt="" />
-                                        <p>3.0</p>
-                                    </div>
-                                </div> */}
-
                 </div>
-                <div id='div_resultado'>
-                    <div className='text_match'>Match</div>
-                    <div className='div_circulo'>
-                        <div className='circulo'><p>90%</p></div>
-                        <div className='imagens'>
-                            <img src={aceitar} alt="" />
-                            <img src={rejeitar} alt="" /></div>
+                <div className='div_aceitar'>
+                    <div className='div_text'>
+                    <div className='text_match'>{props.modelo}</div>
+                    <div className='text_match'>{props.data}</div>
                     </div>
+                        <div className='imagens'>
+                            <button className='btn_aceitar' onClick={aceitarMatch}>ACEITAR</button>
+                            <button className='btn_recusar' onClick={retirarMatch}>RECUSAR</button>
+                            </div>
+                    
 
                 </div>
             </div>
--- app/frontend/conture-site/src/pages/Match.jsx
+++ app/frontend/conture-site/src/pages/Match.jsx
@@ -1,37 +1,50 @@
 import Header from "../components/Header"
-import Perfil from "../components/PerfilSimples"
+import Perfil from "../components/Perfil"
 import MenuPerfil from "../components/MenuDisponivel"
 import '../html-css-template/css/Match.css'
+import '../html-css-template/css/CardMatch.css'
 import Footer from "../components/Footer"
 import Card from "../components/CardMatch"
-import { Link } from "react-router-dom";
+import Card2 from "../components/CardMatch"
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import apiProdutos from "../apiProduto.js"
+import apiUsuario from "../apiUsuario.js"
+import fotoLogado from '../html-css-template/imagens/icon-logado-sem-foto.png';
+import estrela from '../html-css-template/imagens/star16.svg';
 
 function Match() {
 
    
     return (
         <>
-            <Header></Header>
+           <Header></Header>
             <section className="section-match">
                 <Perfil></Perfil>
                 <MenuPerfil></MenuPerfil>
                 <div className="div_matchs">
-                <div className="match">
-                <div className='header_matchs'>
-                <p>Notebook i5</p>
-                 </div>  
-                <div className="cards">
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
+                    <div className="text_matchs"><b><p>Matchs</p></b></div>
 
+                    <div className="cards">
+                        <Card
+
+                            foto={fotoLogado}
+
+                            nome={"Flávia"}
+
+                            cidade={"São Paulo"}
+
+                            uf={"SP"}
+
+                            nota={"5.0"}
+
+                            modelo={"Ideapad 3i"}
+                            data={"01/12/2022"}
+                        ></Card>
+
+                    </div>
                 </div>
-                </div>
-</div>
-          
+
             </section>
             <Footer></Footer>
         </>
